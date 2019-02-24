66ba0ac1358b8da1c983dcdb16d135abb812bf1b
Michelle Taylor <mmtaylor3@dmacc.edu>
2019-02-24 04:15:11 -0600

    Initial commit.

---
+++ BlueLightGames.Domain/Entities/HighScore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlueLightGames.Domain.Entities
+{
+    public class HighScore
+    {
+        public Guid HighScoreId { get; set; }
+        public Guid PersonId { get; set; }
+        public float Score { get; set; }
+        public DateTime? DateAttained { get; set; }
+    }
+}
