0000a5ab82619c87a8ac885016d693dde579303a
Michelle Taylor <mmtaylor3@dmacc.edu>
2019-02-27 08:35:25 -0600

    no message

---
+++ BlueLightGames/BlueLightGames.Shared/Orchestrators/PersonOrchestrator.cs
@@ -0,0 +1,29 @@
+﻿using BlueLightGames.Domain;
+using BlueLightGames.Shared.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BlueLightGames.Shared.Orchestrators
+{
+    public class PersonOrchestrator
+    {
+        private readonly BlueLightGamesContext _blueLightGamesContext;
+
+        public PersonOrchestrator()
+        {
+            _blueLightGamesContext = new BlueLightGamesContext();
+        }
+
+        public List<PersonViewModel> GetAllPeople()
+        {
+            var people = _blueLightGamesContext.People.Select(x => new PersonViewModel
+            {
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Email = x.Email
+            }).ToList();
+
+            return people;
+        }
+    }
+}
---
+++ BlueLightGames/BlueLightGames.Web/Api/PersonApiController.cs
@@ -0,0 +1,30 @@
+﻿using BlueLightGames.Shared.Orchestrators;
+using BlueLightGames.Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BlueLightGames.Web.Api
+{
+    [Route("api/v1/people")]
+    public class PersonApiController : ApiController
+    {
+        private readonly PersonOrchestrator _personOrchestrator;
+
+        public PersonApiController()
+        {
+            _personOrchestrator = new PersonOrchestrator();
+        }
+
+        [HttpGet]
+        public List<PersonViewModel> GetAllPeople()
+        {
+            var people = _personOrchestrator.GetAllPeople();
+
+            return people;
+        }
+    }
+}
